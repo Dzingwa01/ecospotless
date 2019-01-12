e15bc92f570c7368d61c11085200a7f30c55f388
Tongai <tongai@avochoc.com>
2019-01-12 16:09:57 +0200

    Home page fixes

--- resources/views/layouts/welcome-layout.blade.php
+++ resources/views/layouts/welcome-layout.blade.php
@@ -25,13 +25,15 @@
 </ul>
 <div class="container-fluid">
     <div class="navbar-fixed">
-        <nav>
+        <nav style="height:64px!important;">
             <div class="nav-wrapper">
                 <a href="#!" class="brand-logo"><img src="{{asset('img/logo.jpg')}}" /></a>
                 <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
                 <ul class="right hide-on-med-and-down">
-                    <li><a href="!#"><i class="material-icons left">info_outline</i>About Us</a></li>
-                    <li><a href="!#"><i class="material-icons left">contact_mail</i>Contact Us</a></li>
+                    <li><a href="#employment-opportunities"><i class="material-icons left">nature_people</i>Opportunities</a></li>
+                    <li><a href="#"><i class="material-icons left">payment</i>Book Now</a></li>
+                    <li><a href="#about-us"><i class="material-icons left">info_outline</i>About Us</a></li>
+                    <li><a href="#contact-us"><i class="material-icons left">contact_mail</i>Contact Us</a></li>
                     <li><a class="dropdown-trigger-cus" href="#!" data-target="dropdown2">Account<i class="material-icons right">arrow_drop_down</i></a></li>
 
                 </ul>
@@ -40,13 +42,52 @@
     </div>
     <ul class="sidenav" id="mobile-demo">
         <li><a href="#"><i class="material-icons left">home</i>Home</a></li>
-        <li><a href="#"><i class="material-icons left">info_outline</i>About Us</a></li>
-        <li><a href="#"><i class="material-icons left">contact_mail</i>Contact Us</a></li>
+        <li><a href="#employment-opportunities"><i class="material-icons left">nature_people</i>Opportunities</a></li>
+        <li><a href="#"><i class="material-icons left">payment</i>Book Now</a></li>
+        <li><a href="#about-us"><i class="material-icons left">info_outline</i>About Us</a></li>
+        <li><a href="#contact-us"><i class="material-icons left">contact_mail</i>Contact Us</a></li>
         <li class="divider"></li>
         <li><a class="dropdown-trigger-c" href="#!" data-target="dropdown1">Account<i class="material-icons right">arrow_drop_down</i></a></li>
     </ul>
 </div>
 @yield('content')
+
+<footer class="page-footer" style=" background-color: #22BC67!important;">
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12" id="about-us">
+                <h5 class="white-text">Company Bio</h5>
+                <p class="grey-text text-lighten-4" style="text-align: justify;font-size: 16px!important;"> We are a black owned company with innovative ways to save the environment and the unemployment crisis. We do not use any Carbon
+                emmiting machinery, therefore our services are 100% environmentally friendly. We are passionate about saving water as it is said by 2025 we can not have fresh water because of climate change.
+                It takes 3 litres of freshwater to produce 1 litre of drinkable water. Ecospotless wash is cost effective, per 1 wash we save more than 150 litres and create 2 jobs.
+                </p>
+
+
+            </div>
+            <div class="col l3 s12">
+                <h5 class="white-text">Settings</h5>
+                <ul>
+                    <li><a class="white-text" href="#!">marketing@ecospotless.co.za</a></li>
+                    <li><a class="white-text" href="#!">+27 84 255 3043</a></li>
+
+                </ul>
+            </div>
+            <div class="col l3 s12">
+                <h5 class="white-text">Connect</h5>
+                <ul>
+                    <li><a class="white-text" href="#!">Facebook</a></li>
+                    <li><a class="white-text" href="#!">Twitter</a></li>
+                    <li><a class="white-text" href="#!">Instagram</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            Made by <a class=" text-lighten-3" style="color:#fff!important;" href="#">MARSHTEQ</a>
+        </div>
+    </div>
+</footer>
 <script
         src="https://code.jquery.com/jquery-3.3.1.min.js"
         integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,14 +9,153 @@
     <li><a class="dropdown-trigger" href="#!" data-target="dropdown2">Account<i class="material-icons right">arrow_drop_down</i></a></li>
 </ul>
     <div class="parallax-container">
-        <div class="parallax"><img src="https://picsum.photos/1000/800/?random"></div>
+        <div class="section no-pad-bot">
+            <div class="container">
+                <br><br>
+                <h1 class="header center" style="color: #1393FF;font-weight:bolder;">Ecospotless Car Wash</h1>
+                <div class="row center">
+                    <h5 class="header col s12" style="font-weight:bolder;color:white!important;">Quality Waterless Car Wash by experts.
+                    </h5>
+                </div>
+                <div class="row center">
+                    <a href="#" id="download-button" class="btn btn-large">Book Now</a>
+                </div>
+                <br><br>
+
+            </div>
+        </div>
+        <div class="parallax"><img src="/img/soft-touch-washing.jpg"></div>
     </div>
     <div class="section white">
         <div class="row container">
-            <h2 class="header">Parallax</h2>
-            <p class="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
+            <h2 class="header" style="text-align: center;">Welcome</h2>
+            <p  style="text-align: justify;color:black!important;font-size: 16px!important;">Washing your vehicle with waterless based techniques is a good step in the right direction
+                to save water and our environment.
+                We use quality commercially tested waterless products that have been successfully used around the globe. Some of the inherent benefits of waterless car wash products are as below:
+                Our services are economical, fast, versatile, reqarding and quality is 100% guaranteed.
+            </p>
+
+
+            {{--<ul style="list-style: circle!important;">--}}
+                {{--<li>Economical - We don't require to carry around water around in big trucks. This drastically reduces our expenses and we inherently lower the cost of our service.</li>--}}
+                {{--<li>Fast - Depending on size of vehicle and condition, an average exterior service can be carried out in 15-20 minutes using between 100ml – 160ml of product. </li>--}}
+                {{--<li>Versatile - Your vehicle can be cleaned at any place and at any time by our mobile teams. The service and results are astounding without causing any disruption or pollution to the surrounding environment,--}}
+                    {{--over-spray to neighbouring vehicles or requiring precious time allocation from the vehicle owner.</li>--}}
+                {{--<li>Rewarding - By using waterless car washing products and techniques we will be saving 1000's of litres of water. </li>--}}
+                {{--<li>Quality - We strive to ensure that we provide urivalled quality service to your satisfaction.</li>--}}
+            {{--</ul>--}}
+        </div>
+        <div class="row container">
+            <h4 style="text-align: center;">Why Waterless Car Wash</h4>
+            <div class="col s12 m6">
+                <div class="card hoverable">
+                    <div class="card-content ">
+                        <span class="card-title" style="color:black!important;font-weight: 400">Traditional Car Wash</span>
+                           <li>Leaves pools of mud and chemicals</li>
+                           <li>Resource Consuming - Water, Electricity & kerosene</li>
+                           <li>Potential waterway pollution throudh contaminated rinse off</li>
+                           <li>Not compatible for indoor operations</li>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m6">
+                <div class="card hoverable">
+                    <div class="card-content">
+                        <span class="card-title" style="color:black!important;font-weight: bold;font-weight: 400">Waterless Car Wash</span>
+                        <li>No Mess or indication that a valeting operation was present</li>
+                        <li>No effect to nearby vehicles, peoples or areas</li>
+                        <li>No pollution, water, air or noise</li>
+                        <li>Indoor friendly</li>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
+<div class="container row">
+    <h4 style="text-align: center;">Pricing</h4>
+    <div class="col s12">
+
+    <table class="">
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Price</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+    </table>
+    </div>
+</div>
+
+<h4 style="text-align: center;">Employment Opportunities</h4>
+<div id="employment-opportunities" class="parallax-container">
+    <div class="section no-pad-bot">
+        <div class="container">
+            <br/>
+            <br/>
+            <div class="row center" style="margin-top:7em;">
+                <h5 class="header col s12" style="font-weight:bolder;color:#fff!important;">Looking for a job as one of our agents?</h5>
+            </div>
+            <div class="row center">
+                <a href="#" id="download-button" class="btn btn-large">Apply Now</a>
+            </div>
+        </div>
+    </div>
+    <div class="parallax"><img src="/img/job-opportunities-logo.jpg" ></div>
+</div>
+
+<div class="row container" id="contact-us">
+    <h4 style="text-align: center;">Contact Us</h4>
+    <div class="col s12 center" >
+        <div class="row">
+            <form class="col s12">
+                <div class="row">
+                    <div class="input-field col m6 col s12">
+                        <i class="material-icons prefix">account_circle</i>
+                        <input id="first_name" type="text" class="validate">
+                        <label for="first_name">First Name</label>
+                    </div>
+                    <div class="input-field col m6 col s12">
+                        <i class="material-icons prefix">account_circle</i>
+                        <input id="last_name" type="text" class="validate">
+                        <label for="last_name">Last Name</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col m6 col s12">
+                        <i class="material-icons prefix">email</i>
+                        <input id="email" type="email" class="validate">
+                        <label for="email">Email</label>
+                    </div>
+                    <div class="input-field col m6 col s12">
+                        <i class="material-icons prefix">phone</i>
+                        <input id="contact_number" type="tel" class="validate">
+                        <label for="contact_number">Contact Number</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <i class="material-icons prefix">mode_edit</i>
+                        <textarea id="message" class="materialize-textarea"></textarea>
+                        <label for="message">Message</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+                            <i class="material-icons right">send</i>
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+    </div>
+</div>
 </div>
 
 @endsection
